e165b10e39dac31576a335533f91808bb64837b0
Esteban Pisani <estebandpisani@gmail.com>
2022-08-11 10:16:59 -0300

    First commit

---
+++ src/main/java/com/alkemy/geoicons/GeoiconsApplication.java
@@ -0,0 +1,13 @@
+package com.alkemy.geoicons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoiconsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoiconsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alkemy/geoicons/GeoiconsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkemy.geoicons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoiconsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

ffb54dea09cf9d68d36d954b19936773d2ac100e
Esteban Pisani <estebandpisani@gmail.com>
2022-08-11 16:15:39 -0300

    icon and country controllers

---
+++ src/main/java/com/alkemy/geoicons/controller/CountryController.java
@@ -0,0 +1,44 @@
+package com.alkemy.geoicons.controller;
+
+import com.alkemy.geoicons.dto.CountryBasicDTO;
+import com.alkemy.geoicons.dto.CountryDTO;
+import com.alkemy.geoicons.service.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController("cities")
+public class CountryController {
+    @Autowired
+    private CountryService countryService;
+
+    @PostMapping
+    public ResponseEntity<CountryDTO> save(@RequestBody CountryDTO dto){
+        CountryDTO newCountry = countryService.save(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newCountry);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CountryDTO> update(@RequestBody CountryDTO dto, @PathVariable Long id) throws Exception {
+        CountryDTO dtoUpdated = countryService.update(dto, id);
+        return ResponseEntity.status(HttpStatus.CREATED).body(dtoUpdated);
+    }
+    @GetMapping()
+    public ResponseEntity<List<CountryBasicDTO>> getAllCountries() throws Exception {
+        List<CountryBasicDTO> dtos = countryService.getAllCountries();
+        return ResponseEntity.status(HttpStatus.OK).body(dtos);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<CountryDTO> getCountryById(@PathVariable Long id) throws Exception {
+        return ResponseEntity.ok().body(countryService.getCountryById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCountryById(@PathVariable Long id){
+        countryService.deleteCountryById(id);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+}
---
+++ src/main/java/com/alkemy/geoicons/controller/IconController.java
@@ -0,0 +1,44 @@
+package com.alkemy.geoicons.controller;
+
+import com.alkemy.geoicons.dto.IconBasicDTO;
+import com.alkemy.geoicons.dto.IconDTO;
+import com.alkemy.geoicons.service.IconService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController("icons")
+public class IconController {
+    @Autowired
+    private IconService iconService;
+
+    @PostMapping
+    public ResponseEntity<IconDTO> save(@RequestBody IconDTO dto){
+        IconDTO newIcon = iconService.save(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newIcon);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<IconDTO> update(@RequestBody IconDTO dto, @PathVariable Long id) throws Exception {
+        IconDTO dtoUpdated = iconService.update(dto, id);
+        return ResponseEntity.status(HttpStatus.CREATED).body(dtoUpdated);
+    }
+    @GetMapping()
+    public ResponseEntity<List<IconBasicDTO>> getAllIcons() throws Exception {
+        List<IconBasicDTO> dtos = iconService.getAllIcons();
+        return ResponseEntity.status(HttpStatus.OK).body(dtos);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<IconDTO> getIconById(@PathVariable Long id) throws Exception {
+        return ResponseEntity.ok().body(iconService.getIconById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteIconById(@PathVariable Long id){
+        iconService.deleteIconById(id);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+}
